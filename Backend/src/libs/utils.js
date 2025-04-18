767474f9c3efa3129acad7ed3f1d98d014faf02f
SarathKalarikkal <sarathkalarikkal0@gmail.com>
2025-04-18 22:44:42 +0530

    completed one

--- Backend/src/libs/utils.js
+++ Backend/src/libs/utils.js
@@ -9,7 +9,7 @@ res.cookie("jwt",token,{
     MaxAge: 5 * 24 * 60 * 60 * 1000, // 5 days
     httpOnly:true,
     sameSite:"strict",
-    secured:process.env.NODE_DEV !== "development",
+    secured:process.env.NODE_ENV !== "development",
 })
 
 return token
